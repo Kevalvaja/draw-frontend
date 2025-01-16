800f89a8035556d86574f01385c3b48a45039c00
keval vaja <kdvaja4848@gmail.com>
2025-01-16 22:34:43 +0530

    changes by keval

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "https://tcp-chat-application-backend.vercel.app"
+export const API_URL = "https://draw-backend.vercel.app"
 const api = axios.create({
     baseURL: API_URL,
     timeout: 20000
